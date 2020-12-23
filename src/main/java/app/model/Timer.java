19456bad7107f84e10ea349ab36ca5e8e6f7abd4
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-24 00:11:35 +0300

    add 2 Beans, 2 @Autowired, @Qualifier

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,16 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+    private Timer timer;
+
+    @Autowired
+    public Timer getTimer() {
+        return timer;
+    }
 
     private Long nanoTime = System.nanoTime();
 
